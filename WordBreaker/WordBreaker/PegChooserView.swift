75da0f9de9dda8f9b450168840f92d60f2c360e6
HAL24 <struligt@gmail.com>
2026-06-26 22:30:31 +0200

    Better PegChooserView with keyboard at bottom and buttons evenly spaced.

--- WordBreaker/WordBreaker/PegChooserView.swift
+++ WordBreaker/WordBreaker/PegChooserView.swift
@@ -14,22 +14,36 @@ struct PegChooserView: View {
     let onChoose: ((Peg) -> Void)?
     //MARK: - Body
     var body: some View {
-        let choicesRows:[[Peg]] = choices.chunked(into: 9)
-        VStack {
-            ForEach(choicesRows, id: \.self) { row in
-                HStack {
-                    ForEach(row, id: \.self) { peg in
-                        Button {
-                            onChoose?(peg)
-                        } label: {
-                            CharView(peg:peg)
+        //TODO: make number of keys in a row dependent on geometry(eg landscape vs portrait)
+        let choicesRows:[[Peg]] = choices.chunked(into: ChoiceLayout.maxNumChoicesPerRow)
+        let rowsCount: CGFloat = CGFloat(choicesRows.count)
+            VStack {
+                ForEach(choicesRows, id: \.self) { row in
+                    HStack {
+                        ForEach(row, id: \.self) { peg in
+                            //TODO: keep sizing the same
+                            Button {
+                                onChoose?(peg)
+                            } label: {
+                                CharView(peg:peg)
+                            }
+                            .frame(width: ChoiceLayout.buttonWidth, height: ChoiceLayout.buttonWidth)
                         }
                     }
                 }
             }
-        }
+            .frame(height: rowsCount * ChoiceLayout.buttonWidth + (rowsCount - 1) * ChoiceLayout.spacing)
     }
 }
+
+struct ChoiceLayout {
+    static let maxNumChoicesPerRow = 9
+    static let spacing: CGFloat = 4
+    static let buttonWidth : CGFloat = 35
+
+    
+}
+
 
 
 extension Array {
